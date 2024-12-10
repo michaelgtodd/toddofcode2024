c3dfd9963eec1af79d53908028214559b8b493f0
Michael Todd <eltodd@gmail.com>
2024-12-09 22:37:45 -0500

    Update template

--- template/src/main.cpp
+++ template/src/main.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
+#include <fstream>
+
+#include <nlohmann/json.hpp>
 
 #include "main.hpp"
 
+using namespace std;
+using namespace nlohmann;
+
 int main (int argc, char* argv[])
 {
-    std::cout << "Blah" << std::endl;
+    std::ifstream input ("../input/massaged_input.txt");
+    // std::ifstream input ("../input/small_massaged_input.txt");
+
+    json js = json::parse(input);
+
+    std::cout << js << std::endl;
 }
