4d43ff07446aae86b4131002d0d4082a49aee751
Michael Todd <eltodd@gmail.com>
2024-12-09 23:05:06 -0500

    Cont cleanup, timing

--- day_2/src/main.cpp
+++ day_2/src/main.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <set>
 #include <list>
+#include <chrono>
 
 #include "main.hpp"
 #include "nlohmann/json.hpp"
@@ -100,6 +101,8 @@ std::list<int> remove_iterator(std::list<int> &list, std::list<int>::iterator re
 
 int main (int argc, char* argv[])
 {
+    auto total_begin = std::chrono::steady_clock::now();
+
     std::ifstream input ("../input/massaged_input.txt");
     // std::ifstream input ("../input/small_massaged_input.txt");
 
@@ -107,7 +110,14 @@ int main (int argc, char* argv[])
 
     std::vector<std::list<int>> list = js["List"];
 
+    float part_one_seconds = 0;
+    float part_two_seconds = 0;
+
+    int p1_value = 0;
+    int p2_value = 0;
+
     {
+        auto part_one_begin = std::chrono::steady_clock::now();
         int passed_reports = 0;
 
         for(auto i : list)
@@ -118,11 +128,14 @@ int main (int argc, char* argv[])
             }
         }
 
-        std::cout << "Part One: Passed reports:\t" << passed_reports << std::endl;
+        auto part_one_end = std::chrono::steady_clock::now();
+        part_one_seconds = std::chrono::duration_cast<std::chrono::microseconds>(part_one_end - part_one_begin).count() / 1000000.0;
 
+        p1_value = passed_reports;
     }
 
     {
+        auto part_two_begin = std::chrono::steady_clock::now();
         int passed_reports = 0;
 
         for(auto i : list)
@@ -153,8 +166,21 @@ int main (int argc, char* argv[])
 
             }
         }
+        auto part_two_end = std::chrono::steady_clock::now();
+        part_two_seconds = std::chrono::duration_cast<std::chrono::microseconds>(part_two_end - part_two_begin).count() / 1000000.0;
 
-        std::cout << "Part Two: Passed reports after dampener:\t" << passed_reports << std::endl;
+        p2_value = passed_reports;
 
     }
+
+    auto total_end = std::chrono::steady_clock::now();
+    float total_seconds = std::chrono::duration_cast<std::chrono::microseconds>(total_end - total_begin).count() / 1000000.0;
+
+    
+    std::cout << "Part One:\t" << p1_value << std::endl;
+    std::cout << "Part Two:\t" << p2_value << std::endl;
+
+    std::cout << "Part One Time: " << part_one_seconds << "s" << std::endl;
+    std::cout << "Part Two Time: " << part_two_seconds << "s" << std::endl;
+    std::cout << "Total Time: " << total_seconds << "s" << std::endl;
 }
